a11c207de12b119819286f0cbcce3b31f4d1bfd8
tsion22girma <tsion22girma@gmail.com>
2024-12-10 10:37:06 -0600

    test sql connection

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,2 +1,15 @@
 import { sql } from '@vercel/postgres'
 
+export async function fetchUsers() {
+    try {
+        
+        const data = await sql`select * from user_test`
+
+        return data.rows
+
+    } catch (error) {
+        console.log('database Error', error)
+        throw new Error('faild to fetch users')
+    }
+    
+}
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { fetchUsers } from '../lib/data'
 
-export default function Page() {
-  return (
-    <div>Page</div>
-  )
+export default async function Page() {
+    const data = await fetchUsers()
+    return (
+        <div>
+            <ul>
+                {data.map((d) => {
+                    return (
+                        <li>{d.name}</li>
+
+                    )
+                })}
+            </ul>
+
+        </div>
+    )
 }
